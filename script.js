85873fca2926d3111f40788c0da9804490ca3015
bzeizinger <bennet.zeizinger@gmail.com>
2025-01-27 14:47:36 +0100

    Design Changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let start_button = document.getElementById("start")
-
 function start() {
     window.location.href = "/pages/intro.html"
+}
+
+function home() {
+    window.location.href = "/index.html"
 }
